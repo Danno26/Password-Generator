df694e0042075c15c0dc9aae951e9130736f9044
Danno26 <55775106+Danno26@users.noreply.github.com>
2020-10-05 01:06:49 -0700

    apparently I made it more complex than needed

--- Develop/script.js
+++ Develop/script.js
@@ -5,74 +5,80 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
-    //possible password values
-    let upCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let lowCase = "abcdefghijklmnopqrstuvwxyz"
-    let includeNums = "1234567890"
-    let specChar = "!@#$%^&*()_+"
     passwordText.value = password;
 }
 
 function generatePassword() {
 
-    // let validation = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+"
 
-    let password = "";
+    //possible password values
+    var up = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var low = "abcdefghijklmnopqrstuvwxyz"
+    var nums = "1234567890"
+    var spec = "!@#$%^&*()_+"
+
+    var password = '';
+
+    var newPassword = [];
 
     //create prompt for password length
-    var lengthAnswer = prompt("Please enter the password length:", '8 - 128 characters');
+    var lengthAnswer = parseInt(prompt("Please enter the password length:", '8 - 128 characters'));
     if (lengthAnswer != null && lengthAnswer != '') {
         if (isNaN(lengthAnswer)) {
-            alert("Must input numerical values between 8 - 128.");
-            return false;
+            return alert("Must input numerical values between 8 - 128.");
+        } else if (lengthAnswer < 8) {
+            return alert("Password must be at least 8 characters and at most 128.")
+        } else if (lengthAnswer > 128) {
+            return alert("Password must be at least 8 characters and at most 128.")
         } else {
-            confirm("You chose " + lengthAnswer + "!");
+            confirm("You chose " + lengthAnswer + "!")
         }
         console.log(lengthAnswer)
     };
     //create prompt for upper case letters
     var upCase = confirm("Would you like to include uppercase letters?")
-    if (upCase != null && upCase != "") {
-        if (upCase != false) {
-            alert("You would like to use UPPERCASE letters!")
-        } else {
-            alert("You would not like to include them.")
-        }
-        console.log(upCase)
-    };
+    if (upCase != false) {
+        (newPassword.push(up))
+    } else {
+        alert("You would NOT like to include uppercase letters.")
+    }
+    console.log(upCase)
+
     //create prompt for lower case letters
-    var lowCase = confirm("Would you like to includee lowercase letters?")
-    if (lowCase != null && lowCase != "") {
-        if (lowCase != false) {
-            alert("You would like to use lowercase letters!")
-        } else {
-            alert("You would not like to include them.")
-        }
-        console.log(lowCase)
-    };
+    var lowCase = confirm("Would you like to include lowercase letters?")
+    if (lowCase != false) {
+        (newPassword.push(low))
+    } else {
+        alert("You would not like to include lowercase letters.")
+    }
+    console.log(lowCase)
+
     //create prompt for numbers
     var includeNums = confirm("Would you like to include numbers?")
-    if (includeNums != null && includeNums != "") {
-        if (includeNums != false) {
-            alert("You would like to use numbers!")
-        } else {
-            alert("You would NOT like to use numbers.")
-        }
-        console.log(includeNums)
-    };
+
+    if (includeNums != false) {
+        (newPassword.push(nums))
+    } else {
+        alert("You would NOT like to use numbers.")
+    }
+    console.log(includeNums)
+
     //create prompt for spec characters
     var specChar = confirm("Would you like to use special characts? ie: !@#$%^&*()_+")
-    if (specChar != null && specChar != "") {
-        if (specChar != false) {
-            alert("You would like to include special characters!")
-        } else {
-            alert("You would NOT like to include special characters.")
-        }
-        console.log(specChar)
-    };
+    if (specChar != false) {
+        (newPassword.push(spec))
+    } else {
+        alert("You would NOT like to include special characters.")
+    }
+    console.log(specChar)
+
     //create for loop to choose password characters
+    var newPassString = newPassword.join("");
+
+
     for (var i = 0; i <= lengthAnswer; i++) {
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(value.length - 1)));
+        var randomPass = newPassString.charAt(Math.floor(Math.random() * newPassString.length));
+        password = password.concat(randomPass);
     }
 
 }
